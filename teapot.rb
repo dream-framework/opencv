507d01cabf83cf607ece23e61d7f305d3fbadb1f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-08 03:53:46 +1300

    infusion.rb -> teapot.rb

--- teapot.rb
+++ teapot.rb
@@ -3,25 +3,29 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.1"
+required_version "0.5"
 
-define_package "opencv-2.4.3" do |package|
-	package.build(:all) do |platform, environment|
-		environment.use in: package.source_path do |config|
+define_target "opencv" do |target|
+	target.install do |environment|
+		environment.use in:(package.path + "opencv-2.4.3") do |config|
 			Commands.run("rm", "-rf", "build")
 			Commands.run("mkdir", "build")
 			
 			Dir.chdir("build") do
 				Commands.run("cmake", "-G", "Unix Makefiles",
-					"-DCMAKE_INSTALL_PREFIX:PATH=#{platform.prefix}",
+					"-DCMAKE_INSTALL_PREFIX:PATH=#{config.install_prefix}",
+					"-DCMAKE_PREFIX_PATH=#{config.install_prefix}",
 					"-DCMAKE_CXX_COMPILER_WORKS=TRUE",
 					"-DCMAKE_C_COMPILER_WORKS=TRUE",
 					"-DBUILD_opencv_legacy=OFF",
 					"-DBUILD_opencv_nonfree=OFF",
 					"-DBUILD_opencv_ts=OFF",
 					"-DBUILD_opencv_ts=OFF",
-					"-DBUILD_opencv_objdetect=off",
+					"-DBUILD_opencv_objdetect=OFF",
 					"-DBUILD_opencv_highgui=OFF",
+					# 3rd party libraries are not needed:
+					"-DBUILD_PNG=OFF",
+					"-DBUILD_JPEG=OFF",
 					"-DBUILD_SHARED_LIBS=OFF",
 					"..")
 				
@@ -30,5 +34,13 @@ define_package "opencv-2.4.3" do |package|
 				Commands.run("make", "install")
 			end
 		end
+	end
+	
+	target.depends :platform
+	target.depends "Library/png"
+	target.depends "Library/jpeg"
+	
+	target.provides "Library/opencv" do
+		append linkflags "-lopencv"
 	end
 end
