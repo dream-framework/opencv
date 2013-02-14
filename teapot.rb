21ff549fc673038b96942429c25cedc309880c44
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-02-15 12:40:57 +1300

    Update to support teapot 0.6

--- teapot.rb
+++ teapot.rb
@@ -3,42 +3,39 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.5"
+required_version "0.6"
 
 define_target "opencv" do |target|
 	target.install do |environment|
-		environment.use in:(package.path + "opencv-2.4.3") do |config|
-			Commands.run("rm", "-rf", "build")
-			Commands.run("mkdir", "build")
+		install_external(package.path, "opencv-2.4.3", environment) do |config, fresh|
+			Commands.run("cmake", "-G", "Unix Makefiles",
+				"-DCMAKE_INSTALL_PREFIX:PATH=#{config.install_prefix}",
+				"-DCMAKE_PREFIX_PATH=#{config.install_prefix}",
+				"-DCMAKE_CXX_COMPILER_WORKS=TRUE",
+				"-DCMAKE_C_COMPILER_WORKS=TRUE",
+				"-DBUILD_opencv_legacy=OFF",
+				"-DBUILD_opencv_nonfree=OFF",
+				"-DBUILD_opencv_ts=OFF",
+				"-DBUILD_opencv_ts=OFF",
+				"-DBUILD_opencv_objdetect=OFF",
+				"-DBUILD_opencv_highgui=OFF",
+				# 3rd party libraries are not needed:
+				"-DBUILD_PNG=OFF",
+				"-DBUILD_JPEG=OFF",
+				"-DBUILD_ZLIB=OFF",
+				"-DBUILD_SHARED_LIBS=OFF",
+				"."
+			) if fresh
 			
-			Dir.chdir("build") do
-				Commands.run("cmake", "-G", "Unix Makefiles",
-					"-DCMAKE_INSTALL_PREFIX:PATH=#{config.install_prefix}",
-					"-DCMAKE_PREFIX_PATH=#{config.install_prefix}",
-					"-DCMAKE_CXX_COMPILER_WORKS=TRUE",
-					"-DCMAKE_C_COMPILER_WORKS=TRUE",
-					"-DBUILD_opencv_legacy=OFF",
-					"-DBUILD_opencv_nonfree=OFF",
-					"-DBUILD_opencv_ts=OFF",
-					"-DBUILD_opencv_ts=OFF",
-					"-DBUILD_opencv_objdetect=OFF",
-					"-DBUILD_opencv_highgui=OFF",
-					# 3rd party libraries are not needed:
-					"-DBUILD_PNG=OFF",
-					"-DBUILD_JPEG=OFF",
-					"-DBUILD_SHARED_LIBS=OFF",
-					"..")
-				
-				Commands.run("make")
-				
-				Commands.run("make", "install")
-			end
+			Commands.make
+			Commands.make_install
 		end
 	end
 	
 	target.depends :platform
 	target.depends "Library/png"
 	target.depends "Library/jpeg"
+	target.depends "Library/z"
 	
 	target.provides "Library/opencv" do
 		append linkflags "-lopencv"
