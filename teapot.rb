79c5bdf2a3b8cc0953bcbad20099e3f6e68c6f67
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-10-15 01:52:22 +1300

    Update for teapot 1.0, supporting opencv 2.4.10

--- teapot.rb
+++ teapot.rb
@@ -3,35 +3,36 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "0.8.0"
+teapot_version "1.0"
 
 define_target "opencv" do |target|
-	target.build do |environment|
-		build_external(package.path, "opencv-2.4.5", environment) do |config, fresh|
-			Commands.run("cmake", "-G", "Unix Makefiles",
-				"-DCMAKE_INSTALL_PREFIX:PATH=#{config.install_prefix}",
-				"-DCMAKE_PREFIX_PATH=#{config.install_prefix}",
-				"-DCMAKE_CXX_COMPILER_WORKS=TRUE",
-				"-DCMAKE_C_COMPILER_WORKS=TRUE",
-				"-DBUILD_opencv_legacy=OFF",
-				"-DBUILD_opencv_nonfree=OFF",
-				"-DBUILD_opencv_java=OFF",
-				"-DBUILD_opencv_ts=OFF",
-				"-DBUILD_opencv_ts=OFF",
-				"-DBUILD_opencv_objdetect=OFF",
-				"-DBUILD_opencv_highgui=OFF",
-				# 3rd party libraries are not needed:
-				"-DBUILD_PNG=OFF",
-				"-DBUILD_JPEG=OFF",
-				"-DBUILD_ZLIB=OFF",
-				"-DBUILD_SHARED_LIBS=OFF",
-				"."
-			) if fresh
-			
-			Commands.make
-			Commands.make_install
-		end
+	target.build do
+		source_files = Files::Directory.join(target.package.path, "opencv-2.4.10")
+		cache_prefix = Files::Directory.join(environment[:build_prefix], "opencv-2.4.10-#{environment.checksum}")
+		package_files = Path.join(environment[:install_prefix], "lib/pkgconfig/opencv-2.4.10.pc")
+		
+		cmake source: source_files, build_prefix: cache_prefix, arguments: [
+			"-DCMAKE_CXX_COMPILER_WORKS=TRUE",
+			"-DCMAKE_C_COMPILER_WORKS=TRUE",
+			"-DBUILD_opencv_legacy=OFF",
+			"-DBUILD_opencv_nonfree=OFF",
+			"-DBUILD_opencv_java=OFF",
+			"-DBUILD_opencv_ts=OFF",
+			"-DBUILD_opencv_ts=OFF",
+			"-DBUILD_opencv_objdetect=OFF",
+			"-DBUILD_opencv_highgui=OFF",
+			# 3rd party libraries are not needed:
+			"-DBUILD_PNG=OFF",
+			"-DBUILD_JPEG=OFF",
+			"-DBUILD_ZLIB=OFF",
+			"-DBUILD_SHARED_LIBS=OFF",
+		]
+		
+		make prefix: cache_prefix, package_files: package_files
 	end
+	
+	target.depends "Build/Make"
+	target.depends "Build/CMake"
 	
 	target.depends :platform
 	target.depends "Language/C++11"
@@ -50,4 +51,15 @@ define_configuration "opencv" do |configuration|
 	
 	configuration.require "png"
 	configuration.require "jpeg"
+end
+
+define_configuration "local" do |configuration|
+	configuration[:source] = "https://github.com/dream-framework/"
+	
+	configuration.require "png"
+	configuration.require "jpeg"
+	configuration.require "build-make"
+	configuration.require "build-cmake"
+	configuration.require "build-files"
+	configuration.require "platforms"
 end
