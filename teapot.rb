f4168b68a45a6938bd71264097cb998f9d8445de
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-06-11 02:22:44 +1200

    Update package to opencv-4.3.

--- teapot.rb
+++ teapot.rb
@@ -3,34 +3,52 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-teapot_version "1.0"
+teapot_version "3.0"
+
+opencv_libraries = [
+	"lib/opencv4/3rdparty/libippicv.a",
+	"lib/opencv4/3rdparty/libippiw.a",
+	"lib/opencv4/3rdparty/libittnotify.a",
+	"lib/libopencv_core.a",
+	"lib/libopencv_calib3d.a",
+	"lib/libopencv_features2d.a",
+	"lib/libopencv_flann.a",
+	"lib/libopencv_imgcodecs.a",
+	"lib/libopencv_imgproc.a",
+	"lib/libopencv_ml.a",
+	"lib/libopencv_objdetect.a",
+	"lib/libopencv_photo.a",
+	"lib/libopencv_videoio.a"
+]
 
 define_target "opencv" do |target|
-	OPENCV_LIBRARIES = [
-		"share/OpenCV/3rdparty/lib/libippicv.a",
-		"lib/libopencv_calib3d.a",
-		"lib/libopencv_core.a",
-		"lib/libopencv_features2d.a",
-		"lib/libopencv_flann.a",
-		# "lib/libopencv_highgui.a",
-		"lib/libopencv_imgcodecs.a",
-		"lib/libopencv_imgproc.a",
-		"lib/libopencv_ml.a",
-		"lib/libopencv_objdetect.a",
-		"lib/libopencv_photo.a",
-		"lib/libopencv_videoio.a"
-	]
+	target.depends "Language/C++11"
 	
-	target.build do
-		source_files = Files::Directory.join(target.package.path, "opencv-3.1.0")
-		cache_prefix = Files::Directory.join(environment[:build_prefix], "opencv-3.1.0-#{environment.checksum}")
-		package_files = Files::Paths.directory(environment[:install_prefix], OPENCV_LIBRARIES)
+	target.depends "Library/png"
+	target.depends "Library/jpeg"
+	target.depends "Library/webp"
+	
+	target.depends "Library/z"
+	target.depends "Library/m"
+	target.depends "Library/dl"
+	
+	target.depends :opencv_platform
+	
+	target.depends "Build/Make"
+	target.depends "Build/CMake"
+	
+	target.provides "Library/opencv" do
+		source_files = target.package.path + "opencv-4.3"
+		cache_prefix = environment[:build_prefix] / environment.checksum + "opencv"
+		package_files = cache_prefix.list(*opencv_libraries)
 		
-		cmake source: source_files, build_prefix: cache_prefix, arguments: [
+		cmake source: source_files, install_prefix: cache_prefix, arguments: [
+			"-DBUILD_SHARED_LIBS=OFF",
 			"-DCMAKE_CXX_COMPILER_WORKS=TRUE",
 			"-DCMAKE_C_COMPILER_WORKS=TRUE",
 			"-DBUILD_opencv_java=OFF",
-			"-DBUILD_opencv_ts=OFF",
+			"-DBUILD_opencv_python2=OFF",
+			"-DBUILD_opencv_python3=OFF",
 			"-DBUILD_opencv_ts=OFF",
 			#"-DBUILD_opencv_objdetect=OFF",
 			"-DBUILD_opencv_highgui=OFF",
@@ -42,42 +60,23 @@ define_target "opencv" do |target|
 			"-DWITH_WEBP=OFF",
 			"-DWITH_JASPER=OFF",
 			"-DWITH_TIFF=OFF",
-			"-DBUILD_SHARED_LIBS=OFF",
-		]
+		], package_files: package_files
 		
-		make prefix: cache_prefix, package_files: package_files
+		append linkflags package_files
+		append header_search_paths cache_prefix + "include/opencv4"
 	end
+end
+
+define_configuration "development" do |configuration|
+	configuration[:source] = "https://github.com/dream-framework/"
 	
-	target.depends "Build/Make"
-	target.depends "Build/CMake"
+	configuration.import "opencv"
 	
-	target.depends :platform
-	target.depends "Language/C++11"
+	configuration.require "build-make"
+	configuration.require "build-cmake"
+	configuration.require "build-files"
 	
-	target.depends :opencv_platform
-	
-	target.depends "Library/png"
-	target.depends "Library/jpeg"
-	target.depends "Library/z"
-	target.depends "Library/dl"
-	
-	target.provides "Library/opencv" do
-		append linkflags [
-			->{install_prefix + "share/OpenCV/3rdparty/lib/libippicv.a"},
-			->{install_prefix + "lib/libopencv_calib3d.a"},
-			->{install_prefix + "lib/libopencv_core.a"},
-			->{install_prefix + "lib/libopencv_features2d.a"},
-			->{install_prefix + "lib/libopencv_flann.a"},
-			# ->{install_prefix + "lib/libopencv_highgui.a"},
-			->{install_prefix + "lib/libopencv_imgproc.a"},
-			->{install_prefix + "lib/libopencv_imgcodecs.a"},
-			->{install_prefix + "lib/libopencv_ml.a"},
-			->{install_prefix + "lib/libopencv_objdetect.a"},
-			->{install_prefix + "lib/libopencv_photo.a"},
-			->{install_prefix + "lib/libopencv_video.a"},
-			->{install_prefix + "lib/libopencv_videoio.a"},
-		]
-	end
+	configuration.require "platforms"
 end
 
 define_configuration "opencv" do |configuration|
@@ -85,6 +84,7 @@ define_configuration "opencv" do |configuration|
 	
 	configuration.require "png"
 	configuration.require "jpeg"
+	configuration.require "webp"
 	
 	# Provides suitable packages for building on windows:
 	host /linux/ do
@@ -94,15 +94,4 @@ define_configuration "opencv" do |configuration|
 	host /darwin/ do
 		configuration.require "opencv-darwin"
 	end
-end
-
-define_configuration "local" do |configuration|
-	configuration[:source] = "https://github.com/dream-framework/"
-	
-	configuration.require "png"
-	configuration.require "jpeg"
-	configuration.require "build-make"
-	configuration.require "build-cmake"
-	configuration.require "build-files"
-	configuration.require "platforms"
 end
